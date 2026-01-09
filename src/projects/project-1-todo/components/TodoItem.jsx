5e53fc9c1d4b66a931fa5abcb1c0f0bbb357aeb4
Seth Nkwo <sethnkwocool@gmail.com>
2026-01-09 22:52:15 +0100

    Created TodoItem

--- src/projects/project-1-todo/components/TodoItem.jsx
+++ src/projects/project-1-todo/components/TodoItem.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { useTodos } from '../hooks/useTodos'
+import { ACTIONS } from '../reducer/todoReducer';
 
-export default function TodoItem() {
+export const TodoItem = React.memo(function TodoItem({ id, title, description, completed }) {
+
+    const { dispatch } = useTodos();
+
+    function toggleCompleted() {
+        dispatch({ type: ACTIONS.TOGGLE_TODO, payload: id })
+    }
+
+    function deleteTask() {
+        dispatch({ type: ACTIONS.DELETE_TODO, payload: id })
+    }
     return (
-        <div>TodoItem</div>
+        <div className="task-card">
+            <div>
+                <input checked={completed} onChange={toggleCompleted} type="checkbox" />
+            </div>
+            <div>
+                <p className='font-bold text-2xl'>{title}</p>
+                <p className='text-base'>{description}</p>
+            </div>
+            <div>
+                <button className='deleteBtn' onClick={deleteTask}>Delete</button>
+            </div>
+        </div>
     )
-}
+})
